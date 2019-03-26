0780ae83c7f06e30fddbdf4ded2f22c532a8f463
Lasse <Lasse.BlueBird@web.de>
2019-03-26 11:38:36 +0100

    Refactoring of Day2

--- 2015/day2/Present.cs
+++ 2015/day2/Present.cs
@@ -1,24 +1,46 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace aoc.y2015.day2 {
-    public class Present {
-
-        public int x { get; set;} 
-        public int y { get; set;}
-        public int z { get; set;}
-
-        public Present(string dimensions) {
-            string[] dimensionArray = dimensions.Split('x');
-            this.x = Int32.Parse(dimensionArray[0]);
-            this.y = Int32.Parse(dimensionArray[1]);
-            this.z = Int32.Parse(dimensionArray[2]);
-        }
-        public Present(int x, int y, int z) {
-            this.x = x;
-            this.y = y;
-            this.z = z;
+    public static class Present {
+        
+        public static IEnumerable<int> CalculateSurfaces((int x, int y, int z) tuple) {
+            yield return tuple.x * tuple.y;
+            yield return tuple.y * tuple.z;
+            yield return tuple.x * tuple.z;
+            yield return tuple.x * tuple.y;
+            yield return tuple.y * tuple.z;
+            yield return tuple.x * tuple.z;
         }
 
+        public static int CalculateVolume(IEnumerable<int> sideLengths) =>
+            sideLengths
+                .Aggregate((a, b) => a * b);
+
+        public static int CalculateSmallestScope(IEnumerable<int> sides) {
+            sides = sides
+                .OrderBy(side => side)
+                .Select(side => side * 2);
+            return sides.ElementAt(0) + sides.ElementAt(1);
+        }
+
+        public static IEnumerable<int> GetSideLengthsFromDimensions(string dimensions) =>
+            dimensions
+                .Split('x')
+                .Select(s => Int32.Parse(s));
+
+        public static int CalculateSurface(string dimensions) { 
+            var lengths = GetSideLengthsFromDimensions(dimensions);
+            IEnumerable<int> surfaces = CalculateSurfaces((lengths.ElementAt(0), lengths.ElementAt(1), lengths.ElementAt(2)));
+            return surfaces.Sum() + surfaces.Min();
+        }
+
+        public static int CalculateRibbon(string dimensions) {
+            var lengths = GetSideLengthsFromDimensions(dimensions);
+            return CalculateSmallestScope(lengths) 
+                + lengths.ElementAt(0) * lengths.ElementAt(1) * lengths.ElementAt(2);
+        }
 
     }
 }
--- tests/2015/day2/Day2.cs
+++ tests/2015/day2/Day2.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Xunit;
 using aoc.api;
 using aoc.y2015.day2;
@@ -9,27 +10,37 @@ namespace aoc.test.y2015.day2 {
 
         [Fact]
         public void PartOne() {
-            Present[] presents = GetPresentsFromFile("tests/2015/day2/Presents.Input.txt");
+            string[] presents = GetPresentsFromFile("tests/2015/day2/Presents.Input.txt");
 
-            Console.WriteLine("WrappingPaper: " + PresentCalculator.CalculateWrappingPaper(presents));
+            Assert.Equal(1598415, presents.Select(present => Present.CalculateSurface(present)).Sum());
         }
 
         [Fact]
         public void PartTwo() {
-            Present[] presents = GetPresentsFromFile("tests/2015/day2/Presents.Input.txt");
+            string[] presents = GetPresentsFromFile("tests/2015/day2/Presents.Input.txt");
 
-            Console.WriteLine("Ribbon: " + PresentCalculator.CalculateRibbon(presents));
+            Assert.Equal(3812909, presents.Select(present => Present.CalculateRibbon(present)).Sum());
+        }
+
+        [Theory]
+        [InlineData("2x3x4", 58)]
+        [InlineData("1x1x10", 43)]
+        public void PartOneTest(string presentDimensions, int result) {
+            Assert.Equal(result, Present.CalculateSurface(presentDimensions));
+        }
+
+        [Theory]
+        [InlineData("2x3x4", 34)]
+        [InlineData("1x1x10", 14)]
+        [InlineData("3x3x2", 28)]
+        public void PartTwoTest(string presentDimensions, int result) {
+            Assert.Equal(result, Present.CalculateRibbon(presentDimensions));
         }
 
     
-        private Present[] GetPresentsFromFile(string path) {
-            string[] presentsString = FileReader.ReadFile(path);
-            Present[] presents = new Present[presentsString.Length];
-            for(int i = 0; i < presentsString.Length; i++) {
-                presents[i] = new Present(presentsString[i]);
-            }
-            return presents;
-        }
+        private string[] GetPresentsFromFile(string path) =>
+            FileReader.ReadFile(path);
+
 
 
     }
